71c47aea5ab6ea6140084984c22ddcacb978b229
romatorres <romatorres12@gmail.com>
2025-02-12 21:06:25 -0300

    erros vercel

--- src/app/api/users/upload-image/ver-route.ts
+++ src/app/api/users/upload-image/ver-route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+/* import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { prisma } from "@/lib/prisma";
@@ -71,4 +71,4 @@ export async function POST(request: Request) {
       { status: 500 }
     );
   }
-} 
+}  */
