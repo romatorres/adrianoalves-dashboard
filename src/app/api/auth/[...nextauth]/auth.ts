f642590b7e9ab18d660fbea92978260dd876c756
romatorres <romatorres12@gmail.com>
2025-02-17 17:06:59 -0300

    login9

--- src/app/api/auth/[...nextauth]/auth.ts
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -53,6 +53,22 @@ export const authOptions: AuthOptions = {
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.role = user.role;
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.role = token.role;
+        session.user.id = token.id;
+      }
+      return session;
+    },
+  },
   pages: {
     signIn: "/login",
   },
