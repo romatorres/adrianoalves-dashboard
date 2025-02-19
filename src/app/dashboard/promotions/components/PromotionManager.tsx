311eb08882a6caa8f62a338674b0d07212218392
romatorres <romatorres12@gmail.com>
2025-02-19 17:53:01 -0300

    ui/button

--- src/app/dashboard/promotions/components/PromotionManager.tsx
+++ src/app/dashboard/promotions/components/PromotionManager.tsx
@@ -110,9 +110,7 @@ export default function PromotionManager({
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="flex justify-between items-center mb-8">
-        <h1 className="text-2xl font-bold text-background">
-          Gerenciar Promoções
-        </h1>
+        <h1 className="text-2xl font-bold text-background">Promoções</h1>
         <div className="flex gap-3">
           <ButtonForm variant="btn_icon">
             <Link href="/dashboard">
